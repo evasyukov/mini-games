6ccd4fdcde76da8a64c6a9cae4d241db62815797
evasyukov <egor.vasyukov1@gmail.com>
2024-11-29 20:49:08 +0700

    - сделан список игр - изменен шрифт приложения

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -11,7 +11,14 @@
       <h2>Список игр</h2>
     </div>
 
-    <div class="home__games"></div>
+    <div class="home__games">
+      <div class="home__game" v-for="game in gameList" :key="game.id">
+        <p class="name"> {{ game.name }}</p>
+        <router-link :to="`/${ game.path}`">
+          <button @click="switcherList">Играть</button>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,12 +28,12 @@ import { ref } from "vue"
 const gameList = ref([
   {
     id: 1,
-    title: "Найди число",
+    name: "Найди число",
     path: "guess-number",
   },
   {
     id: 2,
-    title: "Виселица",
+    name: "Виселица",
     path: "gallows",
   },
 ])
@@ -34,14 +41,58 @@ const gameList = ref([
 
 <style scoped lang="scss">
 .home {
+  width: 100%;
+
   &__title {
     text-align: center;
   }
 
   &__header {
+    border-bottom: 3px solid #000;
   }
 
   &__games {
+    width: 100%;
+
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  &__game {
+    width: 230px;
+    height: 250px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+
+    margin: 10px;
+
+    border: 1px solid #000;
+
+    p {
+      margin: 0;
+    }
+
+    button {
+      padding: 5px 20px;
+
+      font-size: 16px;
+      color: #000;
+      background: none;
+
+      border: 1px solid #000;
+
+      cursor: pointer;
+
+      transition: all 0.5s ease 0s;
+
+      &:hover {
+        background: #000;
+        color: #fff;
+      }
+    }
   }
 }
 </style>
